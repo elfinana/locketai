645c90b91ecc47a54d6d9fa036916ba0a41d8d42
elfinana <sunah122828@naver.com>
2025-05-22 15:33:16 +0900

    feat: 말풍선 텍스트 위치 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,54 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="w-full flex justify-center bg-white">
-      <div className="w-full max-w-md  bg-[#F3F2EF]">
+      <div className="w-full max-w-md  bg-[#F3F2EF] relative">
         <section className="w-full">
           <Image
-            src="/assets/up.svg"
+            src="/assets/image.svg"
             alt="상단 이미지"
             width={448}
-            height={652}
-            className="w-full h-auto"
+            height={1403}
+            className="w-full h-full"
           />
-          <Image
-            src="/assets/middle.svg"
-            alt="중간간 이미지"
-            width={448}
-            height={285}
-            className="w-full mt-[120px]"
-          />
-          <Image
-            src="/assets/down.svg"
-            alt="하단 이미지"
-            width={448}
-            height={306}
-            className="w-full mt-10"
-          />
+          <div
+            className="absolute"
+            style={{
+              top: "47%",
+
+              left: "20%",
+
+              width: "29.9%",
+              height: "5.1%",
+            }}
+          >
+            <p
+              className="text-center text-[clamp(8px,3.5vw,16px)] leading-[150%]"
+              style={{ letterSpacing: "-0.4px" }}
+            >
+              이제 본격적으로
+              <br /> OO님의 사주팔자를
+              <br />
+              분석해볼 차례네요.
+            </p>
+          </div>
+          <div
+            className="absolute"
+            style={{
+              top: "73.2%",
+              left: "18.2%",
+              width: "40%",
+              height: "3.4%",
+            }}
+          >
+            <p
+              className="text-center text-[clamp(8px,3.5vw,16px)] leading-[150%]"
+              style={{ letterSpacing: "-0.4px" }}
+            >
+              제가 OO님의 사주를
+              <br />
+              보기 쉽게 표로 정리했어요
+            </p>
+          </div>
         </section>
       </div>
     </main>
